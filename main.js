e279d180fb5fbe25a4d73335771711896153d2d2
YogeshPrasanna <yogesh15031993@gmail.com>
2017-04-07 22:46:17 +0530

    add scores content , add sidemenu current display

--- main.js
+++ main.js
@@ -3,4 +3,39 @@ $(document).ready(function() {
         $("html, body").animate({ scrollTop: 500 }, 600);
         return false;
     })
+
+    $('#about-content').on('mouseenter',function(){
+    	$('#item1').addClass('entered')
+    });
+    $('#about-content').on('mouseleave',function(){
+    	$('#item1').removeClass('entered')
+    });
+
+    $('#scores-content').on('mouseenter',function(){
+    	$('#item2').addClass('entered')
+    });
+    $('#scores-content').on('mouseleave',function(){
+    	$('#item2').removeClass('entered')
+    });
+
+    $('#contribution-content').on('mouseenter',function(){
+    	$('#item3').addClass('entered')
+    });
+    $('#contribution-content').on('mouseleave',function(){
+    	$('#item3').removeClass('entered')
+    });
+
+    $('#awards-content').on('mouseenter',function(){
+    	$('#item4').addClass('entered')
+    });
+    $('#awards-content').on('mouseleave',function(){
+    	$('#item4').removeClass('entered')
+    });
+
+    $('#Life-Story-content').on('mouseenter',function(){
+    	$('#item5').addClass('entered')
+    });
+    $('#Life-Story-content').on('mouseleave',function(){
+    	$('#item5').removeClass('entered')
+    });
 });
